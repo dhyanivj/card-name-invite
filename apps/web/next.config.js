2575763eb7d1c7a5abb15bdfedcab99c0705fd9b
dhyaniv <vijay97dhyani@gmail.com>
2026-03-07 11:43:04 +0530

    vercel config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['placehold.co'], // Add supabase domain here later
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.supabase.co',
+            },
+        ],
     },
 }
 
